7ceb3811717ebb3cdf0331733dc709f59686d71a
4reverSpace <72638060+4reverSpace@users.noreply.github.com>
2020-11-02 18:56:00 +0700

    Update Практика.py

--- 02.11/Практика.py
+++ 02.11/Практика.py
@@ -3,6 +3,5 @@ import pymorphy2
 a=re.split("\b\w+\b",input(),maxsplit=0)
 #или без знаков -a=input().split() 
 for i in a:
-    print(i,"1")
     print((pymorphy2.MorphAnalyzer()).parse(i))#разбор
     print(morph.parse(i)[0].normal_form)#обычная форма
